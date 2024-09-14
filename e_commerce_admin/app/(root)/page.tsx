d2b412e8a22c355d7a68912c2d1aac93f06e8f63
vutuankien <vutuankien030904@gmail.com>
2024-09-14 20:29:07 +0700

    feature:admin clerk authentication

---
+++ e_commerce_admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      
+    </div>
+  )
+}
+
+export default SetupPage;
---
+++ e_commerce_admin/app/layout.tsx
@@ -0,0 +1,31 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedOut>
+              {/* <SignInButton /> */}
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
